92296b33f8f0abb119e30b785e0d90fbb94ee097
Jake <jake@distantridge.com>
2021-12-16 14:48:15 +0000

    Quick and Dirty

---
+++ esp.c.ino
@@ -0,0 +1,22 @@
+#define UPDATE_FREQUENCY 1;
+#define GAME_IP "192.168.50.122";
+#define DATA_AVERAGE_COUNT 3;
+
+float speedDataArr[DATA_AVERAGE_COUNT];
+short speedDataIndex = 0;
+float calculatedSpeed;
+
+void setup() {
+  //Join Network
+  //Setup interupt
+}
+
+void loop() {
+  float runningTotal = 0;
+  for (short i = 0; i < DATA_AVERAGE_COUNT; i++) {
+    runningTotal += speedDataArr[i];
+  }
+  calculatedSpeed = runningTotal / DATA_AVERAGE_COUNT;
+  print String(calculatedSpeed)
+  delay(int((1/120) * 1000);
+}
